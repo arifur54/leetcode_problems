5f8bae597a7e068fe4d33430da57f8723215ac88
Arifur Rahman <arif.r.92@gmail.com>
2024-04-30 13:49:57 -0400

    most words added

--- Easy/Arrays/mostWordsFound.js
+++ Easy/Arrays/mostWordsFound.js
@@ -1,16 +1,21 @@
 const sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
 
 function mostWordsFound(sentences) {
-    // let result = []
-    // // loop through the sentence
     // sentences.forEach(sentence => {
     //     // each sentence split the sentence between _____ .
-    //     let splitedSentence = sentence.split(" ").length
-    //     console.log(splitedSentence)
-    //     result.push(splitedSentence)
+    //     Math.max(sentence.map(el => {
+    //         console.log(el)
+    //     }))
     // });
-
-    // return Math.max(...result.map(elment => elment.length))
+    return Math.max(...sentences.map(el => {
+        conuter = 0;
+        for(let e of el){
+            if(e == ' '){
+                conuter++     
+            }
+        }
+        return conuter + 1
+    }))
 }
         
 console.log(mostWordsFound(sentences))
